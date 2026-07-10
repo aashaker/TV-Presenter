2e00c2ba77decf75b639f9a9d4cf0695cf1df40e
aashaker <ahmedayman5209@gmail.com>
2026-07-10 23:33:14 +0300

    v0.5.3

--- details/script.js
+++ details/script.js
@@ -3,34 +3,84 @@ const api_key= 'a94a0ac2bca639fa00cb5b5a58bf2952';
 const urlParams = new URLSearchParams(window.location.search);
 const thingID = urlParams.get('id');
 const thingTYPE = urlParams.get('media_type');
-console.log(thingID);
+const caraCAST = document.getElementById("caraCAST");
+let creditsType = 0
 
-const trendingM = document.getElementById("trendingM");
+if(thingTYPE=='tv'){
+  creditsType = 'aggregate_credits'
+}
+else{
+  creditsType = 'credits'
+}
 
-async function getdata() {
+// async function getinfo() {
+//   try {
+//     const response1 = await fetch(
+//       `https://api.themoviedb.org/3/${thingTYPE}/${thingID}?api_key=${api_key}`,
+//     );
+//     if (response1.ok) {
+//       const data = await response1.json();
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+
+  async function  getcast()  {
+
   try {
     const response1 = await fetch(
-      `https://api.themoviedb.org/3/${thingTYPE}/${thingID}?api_key=${api_key}`,
+      
+      `https://api.themoviedb.org/3/${thingTYPE}/${thingID}/${creditsType}?api_key=a94a0ac2bca639fa00cb5b5a58bf2952`
     );
+    console.log(`https://api.themoviedb.org/3/${thingTYPE}/${thingID}/${creditsType}?api_key=a94a0ac2bca639fa00cb5b5a58bf2952`)
+
     if (response1.ok) {
-      trending = await response1.json();
-      trending.results.forEach((movie) => {
-        generateCards(movie.title, movie.poster_path, movie.release_date,movie.id);
+      const castdata = await response1.json();
+      castdata.cast.forEach((person) => {
+        if (thingTYPE =="tv"){
+          let roles=person.roles[0].character
+          for (let i = 1;i < person.roles.length;i++){
+            roles += person.roles[i].character
+          }
+          console.log(roles)
+          
+          castCards(person.name, person.profile_path,roles,person.id)
+        }
+        else{
+          castCards(person.name, person.profile_path, person.character,person.id)
+        }
+        
       });
     }
-  } catch (error) {
+  } 
+  catch (error) {
     console.log(error);
   }
 }
 
-function generateCards(name, image, date,id) {
-  trendingM.innerHTML = trendingM.innerHTML + `        
+
+
+
+
+function castCards(name, image, chara,id) {
+  caraCAST.innerHTML = caraCAST.innerHTML + `        
             <div class="card1" id='${id}' onclick="window.location.href='./details/index.html?id=${id}'">
             <div class="card1img" style="background-image: url('https://image.tmdb.org/t/p/w600_and_h900_face/${image}')"></div>
             <a href="">${name}</a>
-            <p>${date}</p>
+            <p>${chara}</p>
     </div>`;
 }
 
+// function generateCards(name, image, date,id) {
+//   trendingM.innerHTML = trendingM.innerHTML + `        
+//             <div class="card1" id='${id}' onclick="window.location.href='./details/index.html?id=${id}'">
+//             <div class="card1img" style="background-image: url('https://image.tmdb.org/t/p/w600_and_h900_face/${image}')"></div>
+//             <a href="">${name}</a>
+//             <p>${date}</p>
+//     </div>`;
+// }
 
-getdata();
+// getinfo();
+getcast()
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const baseURL = "https://api.themoviedb.org/3";
 
 const trendingM = document.getElementById("trendingM");
+const trendingS = document.getElementById("trendingS");
 
 async function getTrendingM() {
   try {
@@ -10,7 +11,7 @@ async function getTrendingM() {
     if (response1.ok) {
       trending = await response1.json();
       trending.results.forEach((movie) => {
-        generateCards(movie.title, movie.poster_path, movie.release_date,movie.id,movie.media_type);
+        generateCards(movie.title, movie.poster_path, movie.release_date,movie.id,movie.media_type,'trendingM');
       });
     }
   } catch (error) {
@@ -18,8 +19,24 @@ async function getTrendingM() {
   }
 }
 
-function generateCards(name, image, date,id,media_type) {
-  trendingM.innerHTML = trendingM.innerHTML + `        
+async function getTrendingS() {
+  try {
+    const response2 = await fetch(
+      "https://api.themoviedb.org/3/trending/tv/day?api_key=a94a0ac2bca639fa00cb5b5a58bf2952 ",
+    );
+    if (response2.ok) {
+      trending = await response2.json();
+      trending.results.forEach((movie) => {
+        generateCards(movie.name, movie.poster_path, movie.first_air_date,movie.id,movie.media_type,'trendingS');
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function generateCards(name, image, date,id,media_type,cara) {
+  document.getElementById(`${cara}`).innerHTML = document.getElementById(`${cara}`).innerHTML + `        
             <div class="card1" id='${id}' onclick="window.location.href='./details/index.html?media_type=${media_type}&id=${id}'">
             <div class="card1img" style="background-image: url('https://image.tmdb.org/t/p/w600_and_h900_face/${image}')"></div>
             <a href="">${name}</a>
@@ -28,3 +45,4 @@ function generateCards(name, image, date,id,media_type) {
 }
 
 getTrendingM();
+getTrendingS();
