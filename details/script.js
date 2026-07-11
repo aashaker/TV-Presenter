36b1b962531f1b2fed32bba9db9a1149df7ea46d
aashaker <ahmedayman5209@gmail.com>
2026-07-11 23:42:57 +0300

    v0.7.0

--- details/script.js
+++ details/script.js
@@ -7,16 +7,19 @@ const caraCAST = document.getElementById("caraCAST");
 const banner = document.querySelector(".bannerIMG");
 const posterIMG = document.querySelector(".posterIMG");
 const thingName = document.querySelector("#thingName");
+const creators = document.querySelector("#creators");
 let creditsType = 0
 
 if(thingTYPE=='tv'){
-  creditsType = 'aggregate_credits'
+  creditsType = 'aggregate_credits';
+  getinfoS();
 }
 else{
   creditsType = 'credits'
+  getinfoM();
 }
 
-async function getinfo() {
+async function getinfoS() {
   try {
     const response1 = await fetch(
       `https://api.themoviedb.org/3/${thingTYPE}/${thingID}?api_key=${api_key}`
@@ -28,16 +31,48 @@ async function getinfo() {
       banner.style.backgroundImage=`url(https://image.tmdb.org/t/p/w600_and_h900_face/${data.backdrop_path})`
       posterIMG.style.backgroundImage=`url(https://image.tmdb.org/t/p/w600_and_h900_face/${data.poster_path})`
       thingName.innerHTML=data.name
-      phrase.innerHTML=`"" ${data.tagline} ""`
+      phrase.innerHTML=`"" ${data.tagline}""`
       overView.innerHTML =data.overview
       let genrasARR=data.genres
-      let genrasTXT=data.genres[0].name
+      let genrasTXT=`${data.first_air_date} ⦿ ${data.genres[0].name}`
       for (let i = 1;i < genrasARR.length;i++){
       genrasTXT += `, ${genrasARR[i].name}`
-      console.log(genrasTXT)
         }
         genras.innerHTML=genrasTXT
-      console.log(data.genres[0])
+              const creatorsTXT = data.created_by
+      creatorsTXT.forEach(creator=>{
+        generateCreators(creator.name,"creator",creator.id);
+      })
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getinfoM() {
+  try {
+    const response1 = await fetch(
+      `https://api.themoviedb.org/3/${thingTYPE}/${thingID}?api_key=${api_key}`
+    );
+    console.log(`https://api.themoviedb.org/3/${thingTYPE}/${thingID}?api_key=${api_key}`)
+    if (response1.ok) {
+      const data = await response1.json();
+
+      banner.style.backgroundImage=`url(https://image.tmdb.org/t/p/w600_and_h900_face/${data.backdrop_path})`
+      posterIMG.style.backgroundImage=`url(https://image.tmdb.org/t/p/w600_and_h900_face/${data.poster_path})`
+      thingName.innerHTML=data.title
+      phrase.innerHTML=`"" ${data.tagline}""`
+      overView.innerHTML =data.overview
+      let genrasARR=data.genres
+      let genrasTXT=`${data.release_date} ⦿ ${data.genres[0].name}`
+      for (let i = 1;i < genrasARR.length;i++){
+      genrasTXT += `, ${genrasARR[i].name}`
+        }
+      const hours = Math.floor(data.runtime/60)
+      const mineuts= data.runtime % 60
+      genrasTXT = `${genrasTXT} ⦿ ${hours}h ${mineuts}m `;
+      genras.innerHTML=genrasTXT
+
     
     }
   } catch (error) {
@@ -53,6 +88,8 @@ async function getinfo() {
       
       `https://api.themoviedb.org/3/${thingTYPE}/${thingID}/${creditsType}?api_key=a94a0ac2bca639fa00cb5b5a58bf2952`
     );
+    console.log( `https://api.themoviedb.org/3/${thingTYPE}/${thingID}/${creditsType}?api_key=a94a0ac2bca639fa00cb5b5a58bf2952`
+)
 
     if (response1.ok) {
       const castdata = await response1.json();
@@ -63,12 +100,18 @@ async function getinfo() {
           //   roles += person.roles[i].character
           // }
           castCards(person.name, person.profile_path,roles,person.id)
+                
         }
         else{
           castCards(person.name, person.profile_path, person.character,person.id)
         }
-        
-      });
+      })
+            const creatorsTXT = castdata.crew
+      creatorsTXT.forEach(creator=>{
+        if(creator.job=="Director"||creator.job=="Writer"){
+generateCreators(creator.name,creator.job,creator.id)
+        }
+          });
     }
   } 
   catch (error) {
@@ -99,6 +142,13 @@ function castCards(name, image, chara,id) {
 
 }
 
+function generateCreators(name , role , id){
+  creators.innerHTML+=`
+              <div class="creator">
+                <a href='' style='color:#ffffff'id='${id}'>${name}</a>
+                <p>${role}</p>
+            </div>
+  `
+}
 
-getinfo();
 getcast();
