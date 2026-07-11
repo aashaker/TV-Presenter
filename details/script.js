ff1818f4ae32466f0f356b42bd392c3b7cb70c3f
aashaker <ahmedayman5209@gmail.com>
2026-07-11 17:51:51 +0300

    v0.5.8

--- details/script.js
+++ details/script.js
@@ -4,6 +4,9 @@ const urlParams = new URLSearchParams(window.location.search);
 const thingID = urlParams.get('id');
 const thingTYPE = urlParams.get('media_type');
 const caraCAST = document.getElementById("caraCAST");
+const banner = document.querySelector(".bannerIMG");
+const posterIMG = document.querySelector(".posterIMG");
+const thingName = document.querySelector("#thingName");
 let creditsType = 0
 
 if(thingTYPE=='tv'){
@@ -13,18 +16,27 @@ else{
   creditsType = 'credits'
 }
 
-// async function getinfo() {
-//   try {
-//     const response1 = await fetch(
-//       `https://api.themoviedb.org/3/${thingTYPE}/${thingID}?api_key=${api_key}`,
-//     );
-//     if (response1.ok) {
-//       const data = await response1.json();
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+async function getinfo() {
+  try {
+    const response1 = await fetch(
+      `https://api.themoviedb.org/3/${thingTYPE}/${thingID}?api_key=${api_key}`
+    );
+    console.log(`https://api.themoviedb.org/3/${thingTYPE}/${thingID}?api_key=${api_key}`)
+    if (response1.ok) {
+      const data = await response1.json();
+
+      banner.style.backgroundImage=`url(https://image.tmdb.org/t/p/w600_and_h900_face/${data.backdrop_path})`
+      posterIMG.style.backgroundImage=`url(https://image.tmdb.org/t/p/w600_and_h900_face/${data.poster_path})`
+      thingName.innerHTML=data.name
+    
+    
+    
+    
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
   async function  getcast()  {
@@ -34,7 +46,6 @@ else{
       
       `https://api.themoviedb.org/3/${thingTYPE}/${thingID}/${creditsType}?api_key=a94a0ac2bca639fa00cb5b5a58bf2952`
     );
-    console.log(`https://api.themoviedb.org/3/${thingTYPE}/${thingID}/${creditsType}?api_key=a94a0ac2bca639fa00cb5b5a58bf2952`)
 
     if (response1.ok) {
       const castdata = await response1.json();
@@ -44,8 +55,6 @@ else{
           for (let i = 1;i < person.roles.length;i++){
             roles += person.roles[i].character
           }
-          console.log(roles)
-          
           castCards(person.name, person.profile_path,roles,person.id)
         }
         else{
@@ -62,25 +71,27 @@ else{
 
 
 
-
-
 function castCards(name, image, chara,id) {
+  if (image==null){
+    console.log("ostafandy-ification");
+      caraCAST.innerHTML = caraCAST.innerHTML + `        
+            <div class="card1" id='${id}' onclick="window.location.href='./details/index.html?id=${id}'">
+            <div class="card1img" style="background-image: url('../ostafandy.jpg')"></div>
+            <a href="">${name}</a>
+            <p>${chara}</p>
+    </div>`;
+  }
+  else{
   caraCAST.innerHTML = caraCAST.innerHTML + `        
             <div class="card1" id='${id}' onclick="window.location.href='./details/index.html?id=${id}'">
             <div class="card1img" style="background-image: url('https://image.tmdb.org/t/p/w600_and_h900_face/${image}')"></div>
             <a href="">${name}</a>
             <p>${chara}</p>
     </div>`;
+  }
+
 }
 
-// function generateCards(name, image, date,id) {
-//   trendingM.innerHTML = trendingM.innerHTML + `        
-//             <div class="card1" id='${id}' onclick="window.location.href='./details/index.html?id=${id}'">
-//             <div class="card1img" style="background-image: url('https://image.tmdb.org/t/p/w600_and_h900_face/${image}')"></div>
-//             <a href="">${name}</a>
-//             <p>${date}</p>
-//     </div>`;
-// }
 
-// getinfo();
-getcast()
+getinfo();
+getcast();
